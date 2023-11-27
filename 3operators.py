356b38bec4f8c32a4872eb3c2fcbe82bf4053882
matthieupelard13 <150817334+matthieupelard13@users.noreply.github.com>
2023-11-27 15:58:11 +0000

    Add files via upload

---
+++ 3operators.py
@@ -0,0 +1,15 @@
+#3operators.py
+
+# 3.1 Operators
+
+# 3.1.1 Multiply 4 with 8, and print the result.
+# TODO: Write your code below
+result1 = 4 * 8 
+print(result1) 
+
+
+
+# 3.1.2 Divide 30 by 6, and print the result.
+# TODO: Write your code below
+result2 = 30 / 6 
+print(result2) 
