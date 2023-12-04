98304a53e58aae57d0bb677008fe4897f8cb3402
matthieupelard13 <150817334+matthieupelard13@users.noreply.github.com>
2023-12-04 10:48:19 +0000

    Add files via upload

---
+++ mod.py
@@ -0,0 +1,9 @@
+def psum(number1, number2):
+    """Function to print the sum of two numbers"""
+    result_sum = number1 + number2
+    print(f"The sum of {number1} and {number2} is: {result_sum}")
+
+def pmultiply(number1, number2):
+    """Function to print the product of two numbers"""
+    result_multiply = number1 * number2
+    print(f"The product of {number1} and {number2} is: {result_multiply}")
